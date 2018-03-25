f3692e9953fece80c87824009673f2fa9a6be103
森川裕子 <4432yuko@morikawayuukonoMacBook-ea.local>
2018-03-25 11:41:19 +0900

    homework1

--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,22 @@
 
 	
-
+#1から１００までの数をプリントするプログラムを書け。
+#ただし３の倍数の時は数字の代わりに「Fizz」と、5の倍数の時は「Buzz」とプリントし、
+#３と５の倍数の時は「FizzBuzz」とプリントする事
 	
- 10.times do |i|
- 	i = i + 1
-  if  i == 5
-  	puts "5です"
-  else 
-  	puts i
+100.times do |i|
+	i = i + 1
+  if  i %3 == 0 && i %5== 0
+  	puts "FizzBuzz"
+
+  elsif i %3 == 0 
+  	puts "Fizz"
+
+  elsif i %5 == 0
+    puts "Buzz"	
+
+  else
+    puts i	  
   			
   end
- end
+end
